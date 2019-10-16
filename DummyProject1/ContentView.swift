87a7e2aad59ee2415eb32266458e8890c7721b56
dading <dgusti@student.unimelb.edu.au>
2019-10-16 18:05:14 +1100

    Initial Commit

---
+++ DummyProject1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DummyProject1
+//
+//  Created by dading on 16/10/19.
+//  Copyright © 2019 COMP90019. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
